2546d07ba2c69c5f46d1ffe2b711ae0e27494aab
daivikp40 <daivikp40@gmail.com>
2025-12-30 11:34:29 +0530

    https://gearguard-wsig.onrender.com/api

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,3 @@
 // client/src/config.js
-export const API_URL = "http://localhost:5000/api";
+export const API_URL = "https://gearguard-wsig.onrender.com/api";
+// (Make sure to keep the /api at the end)
